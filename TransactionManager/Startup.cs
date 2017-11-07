dddcf0f5b47f2b8edec6e2a8f6d4e6c28aed1ff6
smcoronel <smcoronel@gmail.com>
2017-11-06 23:52:06 -0500

    Add project files.

---
+++ TransactionManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TransactionManager.Startup))]
+namespace TransactionManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
